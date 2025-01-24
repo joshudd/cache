e1618bf2558155d62f093df501ce50c330971c9b
Josh Dickinson <josh25dickinson@gmail.com>
2025-01-24 03:08:10 -0600

    action debug

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // remove static export for middleware support
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '', // for gh pages preview deployments
   images: {
     remotePatterns: [
