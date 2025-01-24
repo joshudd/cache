9555bf6a699b171bb7af89ecc0364340ac7b6286
Josh Dickinson <josh25dickinson@gmail.com>
2025-01-24 00:53:20 -0600

    dynamic url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // enable static exports
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // for gh pages preview deployments
+  images: {
+    unoptimized: true, // required for static export
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*'
+        destination: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/:path*'
       }
     ]
   }
