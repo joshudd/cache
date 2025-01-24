cb9a42d060da31cfcab13fd55fa888cf3e5ac7a3
Josh Dickinson <josh25dickinson@gmail.com>
2025-01-24 03:25:09 -0600

    workflow

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '', // for gh pages preview deployments
+  trailingSlash: true,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ?? '', // ensure assets are loaded from correct path
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
