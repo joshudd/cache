13633140fd96cb55af4915b33a0f14514f479755
Josh Dickinson <josh25dickinson@gmail.com>
2025-01-24 00:01:18 -0600

    jwt app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt',
     'store',
 ]
 
@@ -61,7 +62,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'store.middleware.AuthenticationMiddleware',
     'store.middleware.TrackStatusMiddleware',
-    'rest_framework_simplejwt.middleware.JWTMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
